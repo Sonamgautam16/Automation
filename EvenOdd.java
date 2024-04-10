7a6d09e347dca6bcf9bc694baefbd75d88fb8dfb
Automation <sonamgautam484@gmail.com>
2024-04-10 15:24:58 +0530

    scanner class

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
 class EvenOdd{
 	public static void main(String [] args){
-
+		Scanner sc=new Scanner(System.in);
+		int number=sc.nextInt();
 	int number=25;
 
 	if(number%2==0){
