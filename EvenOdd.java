260b04e38ea821998a309c2c836d2717555df693
Automation <sonamgautam484@gmail.com>
2024-04-10 15:29:47 +0530

    updated code1

--- EvenOdd.java
+++ EvenOdd.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 class EvenOdd{
 	public static void main(String [] args){
 		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your number");
 		int number=sc.nextInt();
-	int number=25;
+
 
 	if(number%2==0){
 	System.out.println("no is even");
