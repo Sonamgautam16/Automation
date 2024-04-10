4e8ad1c06facaa50c4e9b7bcebce09fce7e2f271
Automation <sonamgautam484@gmail.com>
2024-04-10 15:08:51 +0530

    EvenOdd Program

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+class EvenOdd{
+	public static void main(String [] args){
+
+	int number=25;
+
+	if(number%2==0){
+	System.out.println("no is even");
+	}
+
+	else{
+	System.out.println("no is odd");
+	}
+}
+}
