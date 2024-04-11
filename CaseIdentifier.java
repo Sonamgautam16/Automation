e5eef2419d141ecd74b1d04770181e79b9a6e320
Automation <sonamgautam484@gmail.com>
2024-04-11 11:36:20 +0530

    print Stirng First Character

---
+++ CaseIdentifier.java
@@ -0,0 +1,13 @@
+class CaseIdentifier{
+	
+	public static void main(String []args){
+
+
+	int count =100;
+	int test='a';
+
+	System.out.println(test);
+	System.out.println(count);
+	System.out.println(test+count);
+	}
+}
