ba0b89c6a6c8d09761558afeeb0b1d6f401ebf1e
Automation <sonamgautam484@gmail.com>
2024-04-11 13:49:47 +0530

    CaseIdentifier1

---
+++ CaseIdentifier1.java
@@ -0,0 +1,14 @@
+class CaseIdentifier1{
+	
+	public static void main(String [] args){
+
+
+	String input= "Sonam";
+
+	//char firstletter= input.charAt(0);
+	System.out.println(input.charAt(0));
+
+
+	//System.out.println(firstletter);
+	}
+}
